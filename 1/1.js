cd397c037ea80a43b7c040dcbed79dc4db1ef66f
ozyx <jessemazzella@gmail.com>
2018-03-13 09:36:58 -0700

    use method more efficient than brute force

--- 1/1.js
+++ 1/1.js
@@ -1,16 +1,26 @@
 function sumsToK(inputAr, k) {
-    let length = inputAr.length
-    for(let i = 0; i < length; i++){
-        for(let j = 1; j < length; j++){
-            if(inputAr[i] + inputAr[j] == k) {
-                return true
-            }
-        }
+    // sort array least to greatest (area of improvement)
+    inputAr = inputAr.sort(function (a, b) {
+        return a - b
+    });
+
+    let i = 0                  // set i to first element in array
+    let j = inputAr.length - 1 // set j to last element in array
+
+    while (i < j) {
+        if (inputAr[i] + inputAr[j] < k) { i++ }
+        if (inputAr[i] + inputAr[j] > k) { j-- }
+        if (inputAr[i] + inputAr[j] == k) { return true }
     }
     return false
 }
 
+// true case
 let input = [10, 15, 3, 7]
 let k = 17
+console.log(sumsToK(input, k))
 
+// false case
+input = [10, 15, 3, 7]
+k = 27
 console.log(sumsToK(input, k))
