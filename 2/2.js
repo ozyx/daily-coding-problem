97800d081d0e9f26eb3aefd10bd98c39ac854bff
ozyx <jessemazzella@gmail.com>
2018-03-23 15:48:17 -0700

    O(n) solution

--- 2/2.js
+++ 2/2.js
@@ -1,23 +1,34 @@
 function prodArray(input) {
-    let newAr = []
-    let init;
-    let num;
-    for(let i = 0; i < input.length; i++){
-        init = false;
-        for(let j = 0; j < input.length; j++){
-            if(i !== j) {
-                if(init === false){
-                    num = input[j];
-                    init = true;
-                } else {
-                    num *= input[j];
-                }
-            }
-        }
-        newAr.push(num);
+    // declare arrays
+    let left = [];
+    let right = [];
+    let prod = [];
+
+    let n = input.length;
+
+    // leftmost element is always one
+    left[0] = 1;
+
+    // rightmost element is always one
+    right[n - 1] = 1;
+
+    // construct left array
+    for (let i = 1; i < n; i++) {
+        left[i] = input[i - 1] * left[i - 1];
     }
-    return newAr;
+
+    // construct right array
+    for (let i = n - 2; i >= 0; i--) {
+        right[i] = input[i + 1] * right[i + 1];
+    }
+
+    // construct product array
+    for (let i = 0; i < n; i++) {
+        prod[i] = left[i] * right[i];
+    }
+
+    return prod;
 }
 
-let input = [1, 2, 3, 4, 5]
+let input = [3, 2, 1]
 console.log(prodArray(input));
