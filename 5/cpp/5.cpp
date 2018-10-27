151d2154faf1f0e4a21c9865a9ad8bccf3f87785
Omkar Kadam <30336927+omkar8kadam@users.noreply.github.com>
2018-10-27 22:45:04 +0530

    Added solution of 5th problem (#4)

--- 5/cpp/5.cpp
+++ 5/cpp/5.cpp
@@ -1,28 +1,44 @@
 /*
- *    author : Onk_r
- */
-#include <bits/stdc++.h>
-typedef long long ll;
-const ll MOD = 1000000007;
-const ll MAX = 1000010;
+	Author - Omkar Kadam
+*/
+
+#include<iostream>
 using namespace std;
 
-pair <ll,ll> cons(ll a,ll b) {
-    return make_pair(a,b);
+// template is used to make the cons(), car(), and cdr() functions generic i.e to work on any datatype
+
+template <class T1, class T2> 
+pair<T1,T2> cons(T1 a, T2 b)			// Function to construct a pair
+{
+	pair<T1,T2> p;
+	p.first = a;
+	p.second = b;
+	return p;
 }
 
-ll car(pair <ll,ll> p) {
-    return p.first;
+template <class T1, class T2> 
+T1 car(pair<T1,T2> p)					// Function which returns first element of the pair
+{
+	return p.first;
 }
 
-ll cdr(pair <ll,ll> p) {
-    return p.second;
+template <class T1, class T2> 
+T2 cdr(pair<T1,T2> p)					// Function which returns second element of the pair
+{
+	return p.second;
 }
 
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout << car(cons(3,4)) << endl;
-    cout << cdr(cons(3,4)) << endl;
-    return 0;
+int main()
+{
+	cout<<"Constructing pair of two integers:\n";
+	// cons() function constructs and returns a pair
+	cout<<car(cons(3,4))<<endl;			// car() function returns first element of pair
+	cout<<cdr(cons(3,4))<<endl;			// cdr() function returns second element of pair
+	
+
+	cout<<"\nConstructing pair of a double and a character:\n";
+	cout<<car(cons(4.2, 'd'))<<endl;
+	cout<<cdr(cons(4.2, 'd'))<<endl;
+	
+	return 0;
 }
